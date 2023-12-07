b8a442e0cb7f2945f804aa693b6e660240f00c8d
Punarv Dinakar <tranquilblue91@gmail.com>
2023-12-07 22:02:39 +0530

    Mountain removed

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -4,6 +4,5 @@ import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 import BeachCanvas from "./Beach";
 import RobotCanvas from "./Robot";
-import MountainCanvas from "./mountain";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, BeachCanvas, RobotCanvas, MountainCanvas };
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, BeachCanvas, RobotCanvas };
